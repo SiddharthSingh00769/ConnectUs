03095e90ceb762ec9bbee3e98d4787e37cff210e
Siddharth Singh <sid018singh@gmail.com>
2025-07-29 22:19:36 +0530

    started frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
-// import chatRoutes from "./routes/chat.route.js";
+import chatRoutes from "./routes/chat.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -27,7 +27,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-// app.use("/api/chat", chatRoutes);
+app.use("/api/chat", chatRoutes);
 
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static(path.join(__dirname, "../frontend/dist")));
---
+++ frontend/src/pages/OnboardingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OnboardingPage = () => {
+  return (
+    <div>
+      onboard
+    </div>
+  )
+}
+
+export default OnboardingPage
